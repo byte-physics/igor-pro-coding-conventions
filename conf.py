5ea9f489da70890d1fe1ff504b88e3170859899d
Thomas Braun <thomas.braun@byte-physics.de>
2024-09-17 13:33:40 +0200

    conf.py: Fix sphinx error
    This avoids an error with sphinx 8.0.2:
    
    Warning, treated as error: Invalid configuration value found: 'language =
    None'. Update your configuration to a valid language code. Falling back to
    'en' (English).

--- conf.py
+++ conf.py
@@ -24,6 +24,10 @@ master_doc = "index"
 
 html_show_sourcelink = False
 
+# This is also used if you do content translation via gettext catalogs.
+# Usually you set "language" from the command line for these cases.
+language = 'en'
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
